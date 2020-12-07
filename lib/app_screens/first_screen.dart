61c10bbe18297d0e6f72723c1ee001bed2696bee
Rathish Kannan <rathish.kannan@talabat.com>
2020-12-07 10:47:10 +0400

    Update: logic for random number

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class FirstScreen extends StatelessWidget {
   @override
@@ -6,14 +7,21 @@ class FirstScreen extends StatelessWidget {
     return Material(
       color: Colors.lightBlueAccent,
       child: Center(
-        child: Text(
-            "Dear user, currently our systems are under maintenance & we regret the inconvinience caused.",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 14.0,
-                fontWeight: FontWeight.bold)),
+        child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+                "Dear user, currently our systems are under maintenance 🥡 \n \nwe will be back in ${generateRandomNumber()} mins ⏰",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold))),
       ),
     );
+  }
+
+  int generateRandomNumber() {
+    var random = Random();
+    return random.nextInt(10);
   }
 }
