fb57af013b7d9d490aa5927006b0f856eba5ba98
Rathish Kannan <>
2021-01-06 11:35:57 +0400

    Update: path and lib versions

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/rathish.kannan/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/Data/SampleProjects/Flutter/Flutter-KickStarter"
-export "FLUTTER_TARGET=/Volumes/Data/SampleProjects/Flutter/Flutter-KickStarter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/rathish.kannan/Documents/Data/GitHub/Flutter/Flutter-KickStarter"
+export "FLUTTER_TARGET=/Users/rathish.kannan/Documents/Data/GitHub/Flutter/Flutter-KickStarter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
