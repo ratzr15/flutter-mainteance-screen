a7cf44a09acc34687d3bd9c9a2f6680559d8f6c3
Rathish Kannan <>
2021-01-06 11:34:41 +0400

    Remove: files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,1 @@
-// 1. Imports marterial framework
-import "package:flutter/material.dart";
-import 'package:flutter_app/MaintenanceRepoClass.dart';
 
-// 2.1 runApp inflates the widget and shows on screen
-// 2.2 MaterialApp provides general UI elements like AppBar
-void main() => runApp(new MaintenanceRepoClass());
